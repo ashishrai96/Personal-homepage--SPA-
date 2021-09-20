4532074b92e987ae73118f06fc8ba08f7de87784
Ashish Rai <ashishrai96@yahoo.com>
2021-09-20 15:01:36 +0530

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+import Page2 from "./page2";
+
+var view = document.getElementById("main");
+console.log("mainjs loaded");
+
+function OnLoad(){
+    console.log("page loaded");
+
+    // view.innerHTML = Page2.getHtml();
+}
---
+++ scripts/page2.js
@@ -0,0 +1,8 @@
+export class Page2{
+    constructor(){}
+
+    static getHtml = () => `
+        <div>Page 2</div>
+        <button onclick()="alert(\"button 2 clicked\")" >Button 2</button>
+    `
+}
